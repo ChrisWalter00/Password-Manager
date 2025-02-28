1fbc057ceda9263d68299c22b9b2ccc9e5c6843d
ChrisWalter00 <walte178@purdue.edu>
2025-02-28 17:23:51 -0500

    Updating Main

--- main.py
+++ main.py
@@ -7,10 +7,34 @@ def main():
     if op == 1:
         key = None
         while key == None:
-            key = login()
-        log_out = 0
+            key, id = login()
+        log_out = False
         while(not log_out):
-            choice = input("Choose from the List: \n (1) Add password\n (2) Find Password \n (3) Display All Passwords \n (4) Delete Password \n (5) exit\n")
+            choice = int(input("Choose from the List: \n (1) Add password\n (2) Find Password \n (3) Display All Passwords \n (4) Delete Password \n (5) exit\n"))
+            match choice:
+                case 1:
+                    new_pword = input("Enter Password: ")
+                    username = input("Enter Username: ")
+                    purpose = input("Enter the purpose for the password (Amazon, gmail, etc.): ")
+                    encrypted_pword = secure.encrypt(new_pword, key)
+                    mySQL.insert_password(encrypted_pword, username, purpose, id)
+                case 2:
+                    purpose = input("Enter the purpose for the password (Amazon, gmail, etc.): ")
+                    encrypted_pword = mySQL.get_password(purpose, id)
+                    pword = secure.decrypt(encrypted_pword, key)
+                    print(pword)
+                case 3:
+                    encrypted_pword = mySQL.get_all(id)
+                    for i in encrypted_pword:
+                        pword = secure.decrypt(i, key)
+                        print(pword)
+                case 4:
+                    purpose = input("Enter the purpose for the password (Amazon, gmail, etc.): ")
+                    mySQL.delete_password(purpose, id)
+                    print("Password deleted")
+                case 5:
+                    log_out = True
+
     else:
         create_account()
     mySQL.disconnect()
@@ -26,7 +50,7 @@ def login():
         id, username, stored_password, salt = info
         if(secure.verify_pw(stored_password, given_password)):
             print("Successful Login!")
-            return(given_password)
+            return(given_password, id)
     print("Wrong Username/Password")
     return
     
